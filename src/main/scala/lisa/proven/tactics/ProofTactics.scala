8a5cc191a2db4bf6cc03ccbcc35a2a6ed7932c44
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2022-09-28 17:12:57 +0200

    New forall instantiation API

--- src/main/scala/lisa/proven/tactics/ProofTactics.scala
+++ src/main/scala/lisa/proven/tactics/ProofTactics.scala
@@ -16,6 +16,161 @@ object ProofTactics {
 
   def hypothesis(f: Formula): SCProofStep = Hypothesis(emptySeq +< f +> f, f)
 
+  /**
+   * Instantiate universal quantifier
+   *
+   * s1 is a proof of φ (phi), with φ (phi) of the form ∀x.ψ,
+   *
+   * t1 is the proof step number corresponding to s1
+   *
+   * t is the term to instantiate the quantifier with
+   *
+   * <pre>
+   *       Γ ⊢ ∀x.ψ, Δ
+   * -------------------------
+   *     Γ |- ψ[t/x], Δ
+   *
+   * </pre>
+   *
+   * Returns a subproof containing the instantiation steps
+   */
+  def instantiateForall(s1: SCProofStep, t1: Int, phi: Formula, t: Term): SCSubproof = {
+
+    // is the required formula present and well formed?
+    require(s1.bot.right.contains(phi), "Formula ϕ not found in RHS of s1")
+
+    phi match {
+      case psi @ BinderFormula(Forall, _, _) => {
+
+        val tempVar = VariableLabel(freshId(psi.freeVariables.map(_.id), "x"))
+
+        val gamma = s1.bot.left
+        val delta = s1.bot.right - phi
+
+        // instantiate the formula with input
+        val in = instantiateBinder(psi, t)
+
+        // construct proof
+        val p0 = hypothesis(in)
+        val p1 = LeftForall(phi |- in, 0, instantiateBinder(psi, tempVar), tempVar, t)
+        val p2 = Cut((gamma |- (delta + in)), -1, 1, phi)
+
+        /**
+         * in = ψ[t/x]
+         *
+         * s1 = Γ ⊢ ∀x.ψ, Δ       Premise
+         *
+         * p0 = ψ[t/x] ⊢ ψ[t/x]   Hypothesis
+         * p1 = ∀x.ψ ⊢ ψ[t/x]     LeftForall p0
+         * p2 = Γ |- ψ[t/x], Δ    Cut s1, p1
+         *
+         */
+
+        SCSubproof(SCProof(IndexedSeq(p0, p1, p2), IndexedSeq(s1.bot)), Seq(t1))
+      }
+      case _ => require(false, "Formula ϕ is not universally quantified"); SCSubproof(SCProof(), Seq())
+    }
+  }
+
+  /**
+   * Instantiate multiple universal quantifiers
+   *
+   * s1 is a proof of φ (phi), with φ (phi) of the form ∀x1, x2, ..., xn.ψ,
+   *
+   * t1 is the proof step number corresponding to s1
+   *
+   * t* are the terms to instantiate the quantifiers
+   *
+   * Asserts t*.length = n
+   *
+   * Returns a subproof containing individual instantiations as its subproof steps
+   *
+   * <pre>
+   *          Γ ⊢ ∀x1, x2, ..., xn .ψ, Δ
+   * --------------------------------------------
+   *     Γ |- ψ[t1/x1, t2/x2, ..., tn/xn], Δ
+   *
+   * </pre>
+   */
+  def instantiateForall(s1: SCProofStep, t1: Int, phi: Formula, t: Term*): SCSubproof = {
+
+    val p = SCProof(IndexedSeq(), IndexedSeq(s1.bot))
+
+    val pTotal = t.foldLeft((p, s1, -1, phi)) {
+      case ((p, s1, t1, phi), t) => {
+        val pNext = p withNewSteps IndexedSeq(instantiateForall(s1, t1, phi, t))
+        (
+          pNext,
+          pNext.steps.last,
+          p.length,
+          phi match {
+            case psi @ BinderFormula(Forall, _, _) => instantiateBinder(psi, t)
+            case _ => throw new Exception
+          }
+        )
+      }
+    }._1
+
+    SCSubproof(pTotal, Seq(t1))
+  }
+
+  /**
+   * Instantiate universal quantifier, with only one formula in the RHS
+   *
+   * s1 is a proof of φ (phi), with φ (phi) of the form ∀x.ψ,
+   *
+   * Asserts φ (phi) as the sole formula in the RHS of s1's conclusion
+   *
+   * t1 is the proof step number corresponding to s1
+   *
+   * t is the term to instantiate the quantifier with
+   *
+   * <pre>
+   *         Γ ⊢ ∀x.ψ
+   * -------------------------
+   *       Γ |- ψ[t/x]
+   *
+   * </pre>
+   *
+   * Returns a subproof containing the instantiation steps
+   */
+  def instantiateForall(s1: SCProofStep, t1: Int, t: Term): SCSubproof = {
+    // make sure there's only one formula in the RHS of s1
+    require(s1.bot.right.tail.isEmpty, "RHS of s1 is not a singleton set")
+
+    instantiateForall(s1, t1, s1.bot.right.head, t)
+  }
+
+  /**
+   * Instantiate multiple universal quantifiers, with only one formula in the RHS
+   *
+   * s1 is a proof of φ (phi), with φ (phi) of the form ∀x1, x2, ..., xn.ψ,
+   *
+   * Asserts φ (phi) is the sole formula in the RHS of the conclusion of s1
+   *
+   * t1 is the proof step number corresponding to s1
+   *
+   * t* are the terms to instantiate the quantifiers
+   *
+   * Asserts t*.length = n
+   *
+   * Returns a subproof containing individual instantiations as its subproof steps
+   *
+   * <pre>
+   *          Γ ⊢ ∀x1, x2, ..., xn .ψ
+   * --------------------------------------------
+   *      Γ |- ψ[t1/x1, t2/x2, ..., tn/xn]
+   *
+   * </pre>
+   */
+  def instantiateForall(s1: SCProofStep, t1: Int, t: Term*): SCSubproof = {
+    // make sure there's only one formula in the RHS of s1
+    require(s1.bot.right.tail.isEmpty, "RHS of s1 is not a singleton set")
+
+    instantiateForall(s1, t1, s1.bot.right.head, t: _*)
+  }
+
+  @deprecated
   def instantiateForall(p: SCProof, phi: Formula, t: Term): SCProof = { // given a proof with a formula quantified with \forall on the right, extend the proof to the same formula with something instantiated instead.
     require(p.conclusion.right.contains(phi))
     phi match {
@@ -30,6 +185,8 @@ object ProofTactics {
       case _ => throw new Exception("not a forall")
     }
   }
+
+  @deprecated
   def instantiateForall(p: SCProof, phi: Formula, t: Term*): SCProof = { // given a proof with a formula quantified with \forall on the right, extend the proof to the same formula with something instantiated instead.
     t.foldLeft((p, phi)) { case ((p, f), t1) =>
       (
@@ -41,10 +198,15 @@ object ProofTactics {
       )
     }._1
   }
+
+  @deprecated
   def instantiateForall(p: SCProof, t: Term): SCProof = instantiateForall(p, p.conclusion.right.head, t) // if a single formula on the right
+
+  @deprecated
   def instantiateForall(p: SCProof, t: Term*): SCProof = { // given a proof with a formula quantified with \forall on the right, extend the proof to the same formula with something instantiated instead.
     t.foldLeft(p)((p1, t1) => instantiateForall(p1, t1))
   }
+
   def generalizeToForall(p: SCProof, phi: Formula, x: VariableLabel): SCProof = {
     require(p.conclusion.right.contains(phi))
     val p1 = RightForall(p.conclusion -> phi +> forall(x, phi), p.length - 1, phi, x)
